696b7813d5faf57c9b0cb6dd0c93f73d204cd381
tyommmm <artyomhovsepyan135@gmail.com>
2023-09-24 21:16:59 +0400

    Add: ThreadCard, Fetching threads and displaying.

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -36,3 +36,72 @@ export async function createThread({
     throw new Error(`Error creating thread: ${e.message}`);
   }
 }
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  try {
+    connectToDB();
+
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    const postsQuery = Thread.find({
+      parentId: { $in: [null, undefined] },
+    })
+      .sort({ createdAt: "desc" })
+      .skip(skipAmount)
+      .limit(pageSize)
+      .populate({ path: "author", model: User })
+      .populate({
+        path: "children",
+        populate: {
+          path: "author",
+          model: User,
+          select: "_id name parentId image",
+        },
+      });
+
+    const totalPostsCount = await Thread.countDocuments({
+      parentId: { $in: [null, undefined] },
+    });
+
+    const posts = await postsQuery.exec();
+
+    const isNext = totalPostsCount > skipAmount + posts.length;
+
+    return { posts, isNext };
+  } catch (e: any) {
+    throw new Error(`Error creating thread: ${e.message}`);
+  }
+}
+
+export async function fetchThreadById(id: string) {
+  try {
+    connectToDB();
+
+    const thread = await Thread.findById(id)
+      .populate({
+        path: "author",
+        model: User,
+        select: "_id id name image",
+      })
+      .populate({
+        path: "children",
+        populate: [
+          { path: "author", model: User, select: "_id id name parentId image" },
+          {
+            path: "children",
+            model: Thread,
+            populate: {
+              path: "author",
+              model: User,
+              select: "_id id name parentId image",
+            },
+          },
+        ],
+      })
+      .exec();
+
+    return thread;
+  } catch (e: any) {
+    throw new Error(`Error creating thread: ${e.message}`);
+  }
+}
