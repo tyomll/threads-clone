ad569401a420a87d92d012f80f9e9522594b3655
tyommmm <artyomhovsepyan135@gmail.com>
2023-09-24 18:03:03 +0400

    Add: RightSidebar and Bottombar.

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -2,6 +2,9 @@ async function Page() {
   return (
     <main>
       <h1 className="head-text">Onboarding</h1>
+      <p className="mt-3 text-base-regular text-light-2">
+        Complete your profile now to use Threads.
+      </p>
     </main>
   );
 }
